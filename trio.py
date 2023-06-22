ddafd8701baf92b1809ca6aa4e3c0b98668dde3c
yooninsu <insu.yoon@mail.utoronto.ca>
2023-06-22 13:46:58 +0900

    three loops

--- trio.py
+++ trio.py
@@ -1,14 +1,13 @@
-number= [0, 0, 1, -1, 0]
-total = 0
-answer = []
-count = 0
-for i in range(len(number)):
-    for j in range(i+1,len(number)):
-        for k in range(j+1,len(number)):    
-                answer.append([number[i],number[j],number[k]])
-res = list((tuple(sorted(sub)) for sub in answer))
-print(res)
-for num in res:
-    if sum(num) == 0:
-        count +=1
-print(count)
+def solution(number):
+    answer = []
+    count = 0
+    for i in range(len(number)):
+        for j in range(i+1,len(number)):
+            for k in range(j+1,len(number)):    
+                 answer.append([number[i],number[j],number[k]])
+    res = list((tuple(sorted(sub)) for sub in answer))
+    for num in res:
+        if sum(num) == 0:
+            count +=1
+    return count
+
