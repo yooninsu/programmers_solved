fbad255662b70feab749f0cab1895adaa6c3c12e
yooninsu <insu.yoon@mail.utoronto.ca>
2023-07-19 09:51:34 +0900

    counting days

--- 2016.py
+++ 2016.py
@@ -1,17 +1,18 @@
-a = 5
-b = 24
+a = 1
+b = 7
 def solution(a,b):
     dow = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
     daylist = [31,29,31,30,31,30,31,31,30,31,30,31]
-    temp =[]
-
+    temp =[] 
+    count = 0
     for i in range(a):
         temp.append(daylist[i])
         if i == a-1:
             count +=b
         else:
             count = sum(temp)
-        
     for k in range(count):
         result = dow[k%7]
     return result 
+
+solution(1,7)
